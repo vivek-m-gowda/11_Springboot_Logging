fb006ee786b4bf601c37c538da6cb05c84a05648
vivek-m-gowda <vivekgowda880@gmail.com>
2022-09-04 16:34:28 +0530

    Logging first push

---
+++ src/test/java/com/example11/springbootlogging/SpringbootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example11.springbootlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
